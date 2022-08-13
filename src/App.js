194a048f4f25d4cf523de7ad0045906481de2e15
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-08-13 18:49:39 -0300

    create components menu with styled components and material ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 
-import './App.css';
+import styled from "styled-components"
+import Navbar from "./components/Navbar";
+import Menu from "./components/Menu";
+
+const Container = styled.div`
+  display: flex;
+`
+const Main = styled.div`
+  flex: 7;
+` 
+
+const Wrapper = styled.div`
+`
 
 function App() {
   return (
-    <div className="App">
-        <h1>Hello World</h1>
-    </div>
+    <Container>
+        <Menu/>
+          <Main>
+            <Navbar/>
+              <Wrapper>
+                  
+              </Wrapper>
+          </Main>
+    </Container>
+    
   );
 }
 
